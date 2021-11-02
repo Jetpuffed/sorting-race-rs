2a3c0d1a0cc2b9a86a4e8512d5e5510cc19060e4
Jetpuffed <brandon@jetpuffed.org>
2021-11-02 11:29:30 -0700

    Create a random u64 vector generator

--- src/datagen.rs
+++ src/datagen.rs
@@ -7,3 +7,15 @@ pub fn get_rand_u64() -> u64
 
     return val
 }
+
+pub fn get_rand_u64_vec(n: usize) -> Vec<u64>
+{
+    let mut dst: Vec<u64> = Vec::with_capacity(n);
+
+    for _ in 0 .. n
+    {
+        dst.push(get_rand_u64());
+    }
+
+    return dst
+}
