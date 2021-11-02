7097dc07b527afd780e839623129570c776b2c17
Jetpuffed <brandon@jetpuffed.org>
2021-11-02 11:51:26 -0700

    Update insertion_sort to move indexed value in one run

--- src/iterative_sorts.rs
+++ src/iterative_sorts.rs
@@ -1,19 +1,21 @@
 use std::cmp::PartialOrd;
 
-pub fn insertion_sort<T: PartialOrd>(data: &mut Vec<T>)
+pub fn insertion_sort<T: Copy + PartialOrd>(data: &mut Vec<T>)
 {
     let mut i: usize = 1;
 
     while i < data.len()
     {
-        let mut j: usize = i;
+        let x: T = data[i];
+        let mut j: usize = i - 1;
 
-        while (j > 0) && (data[j - 1] > data[j])
+        while (j >= 0) && (data[j] > x)
         {
-            data.swap(j, j - 1);
+            data.swap(j + 1, j);
             j = j.saturating_sub(1);
         }
 
+        data[j + 1] = x;
         i += 1;
     }
 }
