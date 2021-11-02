79c63c2b4cb82016572fc99b7d64133d0b7bd426
Jetpuffed <brandon@jetpuffed.org>
2021-11-02 10:51:37 -0700

    Implement an iterative insertion sort

---
+++ src/iterative_sorts.rs
@@ -0,0 +1,19 @@
+use std::cmp::PartialOrd;
+
+pub fn insertion_sort<T: PartialOrd>(data: &mut [T])
+{
+    let mut i: usize = 1;
+
+    while i < data.len()
+    {
+        let mut j: usize = i;
+
+        while (j > 0) && (data[j - 1] > data[j])
+        {
+            data.swap(j, j - 1);
+            j = j.saturating_sub(1);
+        }
+
+        i += 1;
+    }
+}
