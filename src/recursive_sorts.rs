c1455c8208270ff9437379d99c4c504c0a7aaa1e
Jetpuffed <brandon@jetpuffed.org>
2021-11-02 12:13:19 -0700

    Add comments to the iterative and recursive insertion sorts

--- src/recursive_sorts.rs
+++ src/recursive_sorts.rs
@@ -1,7 +1,15 @@
 use std::cmp::PartialOrd;
 
+/// Recursively iterates and selects an element from the vector, comparing it
+/// against the current largest value in the sorted list behind it. If the
+/// value is larger, it moves on to the next element. If the value is smaller,
+/// it then searches for the correct position to insert that value, shifting
+/// all larger elements to the right by one to make room.
 pub fn insertion_sort<T: Copy + PartialOrd>(data: &mut Vec<T>, n: usize)
 {
+    // Time Complexity => O(n) || O(n^2)
+    // Space Complexity => O(n)
+
     if n > 0
     {
         insertion_sort::<T>(data, n - 1);
