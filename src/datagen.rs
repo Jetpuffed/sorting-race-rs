d58a49d73b3234e841039d88e1ed983bcaa16f5d
Jetpuffed <brandon@jetpuffed.org>
2021-11-02 11:32:42 -0700

    Add comments to the random number generator and vector generator

--- src/datagen.rs
+++ src/datagen.rs
@@ -1,5 +1,6 @@
 use core::arch::x86_64::*;
 
+/// Generates a random `u64` value from the x86 `RDSEED` instruction.
 pub fn get_rand_u64() -> u64
 {
     let mut val: u64 = 0;
@@ -8,6 +9,7 @@ pub fn get_rand_u64() -> u64
     return val
 }
 
+/// Generates an `n` sized vector of random `u64` values.
 pub fn get_rand_u64_vec(n: usize) -> Vec<u64>
 {
     let mut dst: Vec<u64> = Vec::with_capacity(n);
